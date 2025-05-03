f8c3d8978fe560f6c9402eb82a7fc82e6a55bd31
harsha-doddapaneni <harshadoddapaneni1@gmail.com>
2025-05-03 07:49:58 +0530

    python

--- hello.py
+++ hello.py
@@ -24,13 +24,13 @@ Modules
 
 # List and tuples both are not same?
 
-# res = 10 + 20
-# print("Result: ", res)
-# # print(10 + 20)
-# print("Memory Address: ",id(res))
+res = 10 + 20
+print("Result: ", res)
+print(10 + 20)
+print("Memory Address: ",id(res))
 
-# # There are methods specific to that data type 
-# print("Data type: ", type(res))
+# There are methods specific to that data type 
+print("Data type: ", type(res))
 
 """
 this is a block comment
